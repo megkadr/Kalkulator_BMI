68edbf2c3ece104a8e12bf784ec1e3a91a67c3c1
megkadr <39857689+megkadr@users.noreply.github.com>
2022-06-29 17:20:22 +0200

    Add files via upload

--- BMI/BMI.cpp
+++ BMI/BMI.cpp
@@ -1,44 +1,49 @@
 #include <iostream>
-#include <stack>
 #include <vector>
-using namespace std;
-int main() {
-    double w, bmi = 0;
-    float m;
-    unsigned short int t;
-    string name;
-    cin >> t;
-    vector < string > s1;
-    vector < string > s2;
-    vector < string > s3;
-    while (t--) {
-        cin >> name >> m >> w;
-        bmi = (m / (w * w)) * 10000;
-        if (bmi < 18.5)
-        {
-            s1.push_back(name);
-        }
-        else if (bmi > 18.5 && bmi < 25)
-        {
-            s2.push_back(name);
-        }
-        else
-        {
-            s3.push_back(name);
-        }
-    }
-    cout << "niedowaga" << endl;
-    for (int i = 0; i < s1.size(); i++) {
-        cout << s1[i] << endl;
-    }
-    cout << endl << endl;
-    cout << "wartosc prawidlowa" << endl;
-    for (int i = 0; i < s2.size(); i++) {
-        cout << s2[i] << endl;
-    }
-    cout << endl << endl;
-    cout << "nadwaga" << endl;
-    for (int i = 0; i < s3.size(); i++) {
-        cout << s3[i] << endl;
-    }
+#include <string>
+
+int main()
+{
+	double w, bmi = 0;
+	double m;
+	unsigned short int t;
+	std::string name;
+	std::cin >> t;
+	std::vector<std::string> s1;
+	std::vector<std::string> s2;
+	std::vector<std::string> s3;
+	while (t--)
+	{
+		std::cin >> name >> m >> w;
+		bmi = (m / (w * w)) * 10000;
+		if (bmi < 18.5)
+		{
+			s1.push_back(name);
+		}
+		else if (bmi > 18.5 && bmi < 25)
+		{
+			s2.push_back(name);
+		}
+		else
+		{
+			s3.push_back(name);
+		}
+	}
+	std::cout << "niedowaga" << std::endl;
+	for (auto& i : s1)
+	{
+		std::cout << i << std::endl;
+	}
+	std::cout << std::endl << std::endl;
+	std::cout << "wartosc prawidlowa" << std::endl;
+	for (auto& i : s2)
+	{
+		std::cout << i << std::endl;
+	}
+	std::cout << std::endl << std::endl;
+	std::cout << "nadwaga" << std::endl;
+	for (auto& i : s3)
+	{
+		std::cout << i << std::endl;
+	}
 }
